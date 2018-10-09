a1818ee84eb18e51d04a2132de6100f3c50096b7
JoseFerrer <jcfb90@gmail.com>
2018-10-09 11:47:12 -0500

    Change => resolve problem for protected url when user is not loggin

--- App.js
+++ App.js
@@ -1,57 +1,65 @@
-import React, { Component } from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
-import { Redirect } from "react-router";
-import Login from "./src/components/Session/Login/Login";
-import Register from "./src/components/Session/Register/Register";
-import Logout from "./src/components/Session/Logout/Logout";
-import Dashboard from "./src/components/Dashboard/Dashboard";
-import AddContact from "./src/components/Dashboard/components/Contacts/Contacts";
-import Aux from "./src/hoc/Aux";
-import ErrorPage from "./src/errorpages/404/ErrorPage404";
+import React from 'react';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import decode from 'jwt-decode'; // libreria para la verificacion del token que se va a guardar en el localstorage
+import Login from './src/components/Session/Login/Login';
+import Register from './src/components/Session/Register/Register';
+import Logout from './src/components/Session/Logout/Logout';
+import Dashboard from './src/components/Dashboard/Dashboard';
+import AddContact from './src/components/Dashboard/components/Contacts/Contacts';
+import Aux from './src/hoc/Aux'; //
+import ErrorPage from './src/errorpages/404/ErrorPage404';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   loggedin: false
-    // };
-  }
-  // antes de que que se monte el DOM
-  // componentWillMount() {
-  //   let locallogin = localStorage.getItem("auth_token");
-  //   // locallogin = JSON.parse(locallogin);
-  //   if (locallogin != null) {
-  //     this.setState({
-  //       loggedin: true
-  //     });
-  //     this.props.history.push("#/dashboard");
-  //   } else {
-  //     window.location.replace("#/login");
-  //   }
-  //   // } else if (locallogin === null) {
-  //   //   this.setState({
-  //   //     loggedin: false
-  //   //   });
-  //   //   window.location.replace("#/login");
-  //   // }
-  // }
+// Este metodo funciona para poder verificar si lo que se encuentra en localstorage es un token,
+// se encuentra encapsulado en un try - catch para el manejor de los errores, Previamente se debe instalar
+// jwt-decode --> con npm. y se ejecuta llamando al decode + el token asignado a una const donde se guarda el localstorage que viene desde el api
 
-  render() {
-    console.log("render");
-    return (
-      <HashRouter>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/logout" component={Logout} />
-          <Route exact path="/register" component={Register} />
-          {<Route exact path="/dashboard" component={Dashboard} />}
-          <Route exact path="/dashboard/addcontact" component={AddContact} />
-          <Route component={ErrorPage} />
-        </Switch>
-      </HashRouter>
-    );
-  }
-}
+const isAuthenticated = () => {
+	const token = localStorage.getItem('auth_token');
+	try {
+		decode(token);
+	} catch (error) {
+		return false;
+	}
+	return true;
+};
 
-export default App;
+// Este es un Componente que va a utilizar la props de una route para poder renderizar el component protejido
+// Se utiliza el Redirect para poder redirigir en caso que el metodo isAuthenticated falle "no este el token en el localStorage"
+// auth_token
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={props =>
+			isAuthenticated() ? (
+				<Component {...props} />
+			) : (
+				<Redirect
+					to={{
+						pathname: '/login'
+					}}
+				/>
+			)
+		}
+	/>
+);
+
+/* En esta section crea las url del proyecto para poder utilizar el objeto PrivateRoute, el cual va a tener 
+cada una de las propiedad de una route */
+
+export default () => (
+	<HashRouter>
+		<Switch>
+			<Route exact path="/" component={Login} />
+			<Route exact path="/register" component={Register} />
+			<Route exact path="/login" component={Login} />
+			<Route exact path="/logout" component={Logout} />
+			{/* Seccion de rutas protected */}
+			<PrivateRoute path="/dashboard" component={Dashboard} />
+			<PrivateRoute path="/dashboard/addcontact" component={AddContact} />
+			{/* Fin */}
+			<Route component={ErrorPage} />
+		</Switch>
+	</HashRouter>
+);
