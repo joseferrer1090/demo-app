6a04918527798153af22aae7b3adb7c8027e4b1b
root <root@GI-SOFTWARE2.localdomain>
2018-10-10 17:30:14 -0500

    change adding validation

--- src/components/Session/Register/Register.js
+++ src/components/Session/Register/Register.js
@@ -1,88 +1,154 @@
-import React, { Component } from "react";
-import config from "./../../../services/config";
+import React, { Component } from 'react';
+import config from './../../../services/config';
 
 class Register extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            loading: false
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false,
+			fields: [],
+			errors: [],
+			show: true
+		};
+	}
 
-    _handleChange = (e) => {
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
+	handleValidation() {
+		let fields = this.state.fields;
+		let errors = {};
+		let formIsValid = true;
 
-    _handlePostSubmit = () => {
-        this.setState({
-            loading: true
-        })
-        fetch(config.defaultURL + "/api/user/new",{
-            method: "POST",
-            body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password
-            })
-        }).then(response => response.json().then(data => {
-            if(response.ok){
-                alert("Se registro el Usuario con exito");
-                this.props.history.push("/login");
-            }else if(response.status === 422) {
-                console.log(data);
-                alert("no se puedo registrar a la Persona");
-            }
-        }));
-    }
+		// email
+		if (!fields['email']) {
+			formIsValid = false;
+			errors['email'] = 'El campo no puede estar vacio';
+		}
 
-    render(){
-        return(
-            <div>
-                <div className="container"> 
-                <div className="row" style={{ marginTop: "120px" }}>
-                    <div className="col-md-4 col-md-offset-4">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">
-                                <h3 className="panel-title">Registro con Api Carlos</h3>
-                            </div>
-                            <div className="panel-body">
-                                <form role="form" noValidate>
-                                    <fieldset>
-                                        <div className="form-group">
-                                            <input className="form-control"
-                                                placeholder="yourmail@example.com"
-                                                name={"email"}
-                                                type="text"
-                                                value={this.state.email}
-                                                onChange={this._handleChange}
-                                                autoComplete={"Email"}
-                                            />
-                                        </div>
-                                        <div className="form-group">
-                                            <input
-                                                className="form-control"
-                                                placeholder="Regist Password"
-                                                name={"password"}
-                                                type="password"
-                                                value={this.state.password}
-                                                onChange={this._handleChange}
-                                                autoComplete={"Password"}
-                                            />
-                                        </div>
-                                        <button className="btn btn-sm btn-success btn-block " type="button" onClick={ () => {this._handlePostSubmit()} }> Registro </button>
-                                    </fieldset>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        );
-    }
+		if (typeof fields['email'] !== 'undefined') {
+			let lastAtPos = fields['email'].lastIndexOf('@');
+			let lastDotPos = fields['email'].lastIndexOf('.');
+
+			if (
+				!(
+					lastAtPos < lastDotPos &&
+					lastAtPos > 0 &&
+					fields['email'].indexOf('@@') == -1 &&
+					lastDotPos > 2 &&
+					fields['email'].length - lastDotPos > 2
+				)
+			) {
+				formIsValid = false;
+				errors['email'] = 'correo electronico no valido';
+			}
+			this.setState({ errors: errors });
+			return formIsValid;
+		}
+		//Password
+		//Name
+		if (!fields['password']) {
+			formIsValid = false;
+			errors['password'] = 'No puede crear usuario sin password';
+		}
+	}
+
+	handleChange(field, e) {
+		let fields = this.state.fields;
+		fields[field] = e.target.value;
+		this.setState({ fields });
+		//console.log(this.state.fields['password']);
+	}
+
+	_handlePostSubmit = e => {
+		if (this.handleValidation()) {
+			this.setState({
+				loading: true
+			});
+			fetch(config.defaultURL + '/api/user/new', {
+				method: 'POST',
+				body: JSON.stringify({
+					email: this.state.email,
+					password: this.state.password
+				})
+			}).then(response =>
+				response.json().then(data => {
+					if (response.ok) {
+						alert('Se registro el Usuario con exito');
+						this.props.history.push('/login');
+					} else if (response.status === 422) {
+						console.log(data);
+						alert('no se puedo registrar a la Persona');
+					}
+				})
+			);
+		} else {
+			// Notificacion que no se envio el formulario y corregir la informacion que se dijita en el formulario
+		}
+	};
+
+	handleDismiss() {
+		this.setState({
+			show: false
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="container">
+					<div className="row" style={{ marginTop: '120px' }}>
+						<div className="col-md-4 col-md-offset-4">
+							<div className="panel panel-default">
+								<div className="panel-heading">
+									<h3 className="panel-title">Registro con Api Carlos</h3>
+								</div>
+								<div className="panel-body">
+									<form role="form" noValidate>
+										<fieldset>
+											<div className="form-group">
+												<input
+													ref={'email'}
+													className="form-control"
+													placeholder="yourmail@example.com"
+													name={'email'}
+													type="text"
+													value={this.state.fields['email']}
+													onChange={this.handleChange.bind(this, 'email')}
+													autoComplete={'Email'}
+												/>
+												<span> {this.state.errors['email']} </span>
+											</div>
+											<div className="form-group">
+												<input
+													refs={'password'}
+													className="form-control"
+													placeholder="Regist Password"
+													name={'password'}
+													type="password"
+													value={this.state.fields['password']}
+													onChange={this.handleChange.bind(this, 'password')}
+													autoComplete={'Password'}
+												/>
+												<span>{this.state.errors['password']}</span>
+											</div>
+											<button
+												className="btn btn-sm btn-success btn-block "
+												type="button"
+												onClick={() => {
+													this._handlePostSubmit();
+												}}
+											>
+												{' '}
+												Registro{' '}
+											</button>
+										</fieldset>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Register;
